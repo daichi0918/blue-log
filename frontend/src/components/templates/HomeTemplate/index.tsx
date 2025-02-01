a5946e362ef346633f94fc34ebb14ac176cb20a5
daichi0918 <kayasugadaichi@icloud.com>
2025-02-01 11:42:24 +0900

    Footerのコンポーネント分割

--- frontend/src/components/templates/HomeTemplate/index.tsx
+++ frontend/src/components/templates/HomeTemplate/index.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { NotLoginHeader } from "@/components/organisms/NotLoginHeader";
-
 /**
  * HomeTemplate
  *
  * @package templates
  */
+import { Footer } from "@/components/organisms/Footer";
+import { NotLoginHeader } from "@/components/organisms/NotLoginHeader";
+
 import style from "./styles.module.css";
 
 /**
@@ -16,36 +17,8 @@ import style from "./styles.module.css";
 export const HomeTemplate = () => {
   return (
     <>
-      {/* <header className={style.header}>
-        <div>
-          <p>タイトル</p>
-        </div>
-        <div className={style.inputContainer}>
-          <input className={style.input} placeholder={"キーワード検索"} />
-        </div>
-        <div>
-          <button className={`${style.button} ${style.buttonSignup}`}>
-            新規登録
-          </button>
-          <button className={`${style.button} ${style.buttonSignin}`}>
-            ログイン
-          </button>
-        </div>
-      </header> */}
       <NotLoginHeader />
-      <footer className={style.footer}>
-        <div className={style.footerContainer}>
-          <div className={style.footerHome}>
-            <p>HOME</p>
-          </div>
-          <div className={style.footerPrivacyPolicy}>
-            <p>プライバシーポリシー</p>
-          </div>
-          <div className={style.footerTermsOfUse}>
-            <p>利用規約</p>
-          </div>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 };
