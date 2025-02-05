dcca18aec570956d5487d33e97f38eb93f363d3d
daichi0918 <kayasugadaichi@icloud.com>
2025-02-05 19:57:19 +0900

    articleContext途中まで

---
+++ frontend/src/contexts/ArticleContext.ts
@@ -0,0 +1,36 @@
+"use client";
+
+/**
+ * ArticleContext
+ *
+ * @package contexts
+ */
+import type { ReactNode } from "react";
+import { createContext, FC, useContext, useState } from "react";
+import { type ArticleCardType } from "@/type/ArticleCard";
+
+type Props = {
+  children: ReactNode;
+};
+
+type ContextInterface = {
+  articleList: Array<ArticleCardType>;
+};
+
+/**
+ * TodoContext
+ */
+export const ArticleContext = createContext({} as ContextInterface);
+
+/**
+ * ArticleProvider
+ *
+ * @param children
+ */
+// export const ArticleProvider= ({ children }: Props) => {
+//   const [articleList, setArticleList] = useState<Array<ArticleCardType>>([]);
+
+//   return (
+//     <ArticleContext.Provider value={{articleList, setArticleList}}>{children}</ArticleContext.Provider>
+//   );
+// };
