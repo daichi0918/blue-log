cf3d5dd565861cf9e1a58c14eb0482ca0c6385b9
daichi0918 <kayasugadaichi@icloud.com>
2025-02-08 15:20:04 +0900

    AuthContext.tsx実装

---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+/**
+ * AuthContext
+ *
+ * @package contexts
+ */
+import type { FC, ReactNode } from "react";
+import { createContext } from "react";
+import { useAuth } from "@/hooks/useAuth";
+import { type UserType } from "@/type/User";
+
+type Props = {
+  children: ReactNode;
+};
+
+type ContextInterface = {
+  user: UserType | undefined;
+  isAuth: boolean;
+  // why: なぜ引数を入れてる？
+  signIn: (user: UserType) => Promise<void>;
+  signOut: () => Promise<void>;
+};
+
+export const AuthContext = createContext({} as ContextInterface);
+
+/**
+ * AuthProvider
+ * @param children
+ * @returns
+ */
+
+export const AuthProvider: FC<Props> = ({ children }) => {
+  const { user, isAuth, signIn, signOut } = useAuth();
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        isAuth,
+        signIn,
+        signOut,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
