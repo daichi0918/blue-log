85aa50466cc41713fc0a74445cb84bd52d2fc0bc
daichi0918 <kayasugadaichi@icloud.com>
2025-02-11 17:01:53 +0900

    ArticleにHeaderとFooter追加

--- frontend/src/components/templates/ArticleTemplate/index.tsx
+++ frontend/src/components/templates/ArticleTemplate/index.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import { useCallback, useState } from "react";
+import { useParams } from "next/navigation";
+import { Footer } from "@/components/molecules/Footer";
+import { NotLoginHeader } from "@/components/molecules/NotLoginHeader";
+import { useAuth } from "@/hooks/useAuth";
+import { type EventType } from "@/type/Event";
+
 /**
  * ArticleTemplate
  *
@@ -11,5 +18,31 @@
  * @returns {JSX.Element}
  */
 export const ArticleTemplate = () => {
-  return <></>;
+  const param = useParams();
+  console.log("param");
+  console.log(param);
+  // 認証情報を取得
+  const { isAuth, user } = useAuth();
+  const [inputArticleSearch, setInputArticleSearch] = useState<string>("");
+  /* action定義 */
+
+  /**
+   * キーワード検索Input
+   * @param {e}
+   */
+  const handleInputSearch: EventType["onChangeInput"] = useCallback((e) => {
+    setInputArticleSearch(e.target.value);
+  }, []);
+  return (
+    <>
+      <NotLoginHeader
+        user={user}
+        isAuth={isAuth}
+        searchInputValue={inputArticleSearch}
+        handleInputSearch={handleInputSearch}
+      />
+
+      <Footer />
+    </>
+  );
 };
