fc95a8308348d5ff244fa5156b44ea3b6398fcfd
daichi0918 <kayasugadaichi@icloud.com>
2025-02-12 11:39:20 +0900

    記事一覧を降順で取得できるよう修正

--- api-server/src/articles/articles.service.ts
+++ api-server/src/articles/articles.service.ts
@@ -25,7 +25,7 @@ export class ArticlesService {
   async findAll(): Promise<Array<any>> {
     const articles = await this.prismaService.article.findMany({
       orderBy: {
-        createdAt: 'asc', // 昇順（古いものから新しいものへ）
+        createdAt: 'desc',
       },
       select: {
         id: true,
