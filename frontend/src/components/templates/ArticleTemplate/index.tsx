3d710178ed2b40d83520dd1eb443262e8d384d9b
daichi0918 <kayasugadaichi@icloud.com>
2025-02-13 19:48:01 +0900

    add:記事下部のいいね・ブックマーク機能

--- frontend/src/components/templates/ArticleTemplate/index.tsx
+++ frontend/src/components/templates/ArticleTemplate/index.tsx
@@ -120,13 +120,54 @@ export const ArticleTemplate = () => {
                   createdAt={article.createdAt}
                 />
               </div>
+              <ReactMarkdown className={style.markdown}>
+                {article.text}
+              </ReactMarkdown>
+              <div className={style.likeBookmarkContainer}>
+                <div className={style.likeBookmarkWrapper}>
+                  <div className={style.like}>
+                    {article.isLiked ? (
+                      <Image
+                        src="/liked.svg"
+                        alt={"Liked"}
+                        className={style.liked}
+                        width={20}
+                        height={24}
+                      />
+                    ) : (
+                      <Image
+                        src="/notLiked.svg"
+                        alt={"Not Liked"}
+                        className={style.notLiked}
+                        width={20}
+                        height={24}
+                      />
+                    )}
+                    <div className={style.likeCount}>{article.likeCount}</div>
+                  </div>
+                  <div className={style.bookmark}>
+                    {article.isBookmarked ? (
+                      <Image
+                        src="/bookmarked.svg"
+                        alt={"Bookmarked"}
+                        width={14}
+                        height={24}
+                      />
+                    ) : (
+                      <Image
+                        src="/notBookmarked.svg"
+                        alt={"Not Bookmarked"}
+                        width={14}
+                        height={24}
+                      />
+                    )}
+                  </div>
+                </div>
+              </div>
             </>
           ) : (
             <div>もう一度読み込んでください</div>
           )}
-          <ReactMarkdown className={style.markdown}>
-            {article?.text}
-          </ReactMarkdown>
         </section>
         <section className={style.sidebarContainer}>ccc</section>
       </div>
