fc5dfb62b9404d7d9fe46c54d655a5a03c2180d1
daichi0918 <kayasugadaichi@icloud.com>
2025-03-06 16:18:37 +0900

    add:InputFormStories

---
+++ frontend/src/components/atoms/InputForm/InputForm.stories.tsx
@@ -0,0 +1,20 @@
+import { CSSProperties, JSX } from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import { InputForm } from ".";
+
+type InputFormProps = {
+  additionalStyle?: CSSProperties;
+} & JSX.IntrinsicElements["input"];
+
+export default {
+  title: "atoms/InputForm",
+  component: InputForm,
+  tags: ["autodocs"],
+  args: {} as InputFormProps,
+} as Meta;
+
+type Story = StoryObj<typeof InputForm>;
+
+export const DefaultInput: Story = {};
