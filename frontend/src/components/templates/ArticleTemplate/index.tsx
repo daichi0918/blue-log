2341e5bc8a44460f8ed9d2b4ae94c5cce4df7b54
daichi0918 <kayasugadaichi@icloud.com>
2025-03-17 10:46:20 +0900

    fix

--- frontend/src/components/templates/ArticleTemplate/index.tsx
+++ frontend/src/components/templates/ArticleTemplate/index.tsx
@@ -11,12 +11,12 @@ import { Header } from "@/components/layouts/Header";
 import { PageContainer } from "@/components/layouts/PageContainer";
 import { ArticleInfo } from "@/components/molecules/ArticleInfo";
 import { LikeBookmarkButtons } from "@/components/molecules/LikeBookmarkButtons";
+import { MarkdonwPreview } from "@/components/molecules/MarkdonwPreview";
 import { Tags } from "@/components/molecules/Tags";
 import { LoginModal } from "@/components/organisms/LoginModal";
 import { UserCard } from "@/components/organisms/UserCard";
 import { FaFacebook, FaGithub } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import ReactMarkdown from "react-markdown";
 
 import style from "./styles.module.css";
 import { useArticleTemplate } from "./useArticleTemplate";
@@ -112,9 +112,7 @@ export const ArticleTemplate = () => {
                       createdAt={article.createdAt}
                     />
                   </div>
-                  <ReactMarkdown className={style.markdown}>
-                    {article.text}
-                  </ReactMarkdown>
+                  <MarkdonwPreview text={article.text} />
                   <div className={style.likeBookmarkContainer}>
                     <LikeBookmarkButtons
                       isliked={article.isLiked}
