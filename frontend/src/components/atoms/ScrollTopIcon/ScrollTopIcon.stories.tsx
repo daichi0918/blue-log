a4cd58eba3ab692633ca01326bf4fb382cfd339b
daichi0918 <kayasugadaichi@icloud.com>
2025-03-26 08:57:50 +0900

    add

---
+++ frontend/src/components/atoms/ScrollTopIcon/ScrollTopIcon.stories.tsx
@@ -0,0 +1,13 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { ScrollTopIcon } from ".";
+
+export default {
+  title: "atoms/ScrollTopIcon",
+  component: ScrollTopIcon,
+  tags: ["autodocs"],
+} as Meta;
+
+type Story = StoryObj<typeof ScrollTopIcon>;
+
+export const DefaultScrollTopIcon: Story = {};
